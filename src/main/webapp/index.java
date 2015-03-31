0360d289d934b75be227bad49fc05e40ce7a6292
saperovnet <saperovnet@users.noreply.github.com>
2015-03-31 11:16:33 +0300

    Update index.java

--- src/main/webapp/index.java
+++ src/main/webapp/index.java
@@ -1,8 +1,27 @@
+package com.javacodegeeks.corejava.util.logging;
+
+import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-class HelloWorld {
-    private static Logger log = Logger.getLogger(HelloWorld.class.getName());
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        log.info("log Hello World!");
-    }
+
+public class LoggerExample {
+
+	private static final Logger LOGGER = Logger.getLogger(LoggerExample.class.getName());
+	public static void main(String[] args) throws SecurityException, IOException {
+
+		LOGGER.info("Logger Name: "+LOGGER.getName());
+		
+		LOGGER.warning("Can cause ArrayIndexOutOfBoundsException");
+		
+		//An array of size 3
+		int []a = {1,2,3};
+		int index = 4;
+		LOGGER.config("index is set to "+index);
+		
+		try{
+			System.out.println(a[index]);
+		}catch(ArrayIndexOutOfBoundsException ex){
+			LOGGER.log(Level.SEVERE, "Exception occur", ex);
+		}
+	}
 }
